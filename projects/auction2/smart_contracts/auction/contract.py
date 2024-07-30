c3604c1ce4b26b51846b0a16aabd174cb5b06283
quantroy19 <doquan285lg@gmail.com>
2024-07-31 00:40:46 +0700

    feat: start auction

--- projects/auction2/smart_contracts/auction/contract.py
+++ projects/auction2/smart_contracts/auction/contract.py
@@ -1,8 +1,91 @@
-from algopy import ARC4Contract, String
+from algopy import Account, ARC4Contract, Asset, Global, LocalState, Txn, UInt64, gtxn, itxn, op
 from algopy.arc4 import abimethod
 
 
 class Auction(ARC4Contract):
-    @abimethod()
-    def hello(self, name: String) -> String:
-        return "Hello, " + name
+    # Auction
+    # Start Price
+    # Bid - Bid Price >  Start Price
+    # Bid - Bid Price > Previous Bid
+    # Bidder != Previous Bidder
+    # Start Time - End Time
+    # If Auction End -> End Time -> 0
+    # Amount -> Claim
+    def __init__(self) -> None:
+        self.start_time = UInt64(0)  # Auction Start Time
+        self.end_time = UInt64(0)  # Auction End Time
+        self.asa_amount = UInt64(0)  # 1000 - bid price > asa amount
+        self.asa = Asset()  # Token
+        self.previous_bidder = Account()  # Previous Bidder
+        self.claimble = LocalState(UInt64, key="claim", description="The claimle amount")  # Claimble Amount
+
+    # Opt Asset #VBI token
+    @abimethod
+    def opt_into_asset(self, asset: Asset) -> None:  # đưa sản phầm vào đấu giá
+        assert Txn.sender == Global.creator_address, "Only the creator can access"
+        assert self.asa == 0, "ASA already exits"
+
+        self.asa = asset
+        itxn.AssetTransfer(asset_receiver=Global.current_application_address, xfer_asset=asset).submit()
+
+    # Start Auction
+    @abimethod
+    def start_auction(
+        self, start_price: UInt64, length: UInt64, axfer: gtxn.AssetTransferTransaction
+    ) -> None:  # bắt đầu đấu giá
+        assert Txn.sender == Global.creator_address, "Auction must be started by the creator"
+        assert self.end_time == 0, "Auction ended"
+        assert axfer.asset_receiver == Global.current_application_address, "Axfer must be to the current application"
+
+        self.asa_amount = start_price
+        self.end_time = Global.latest_timestamp + length
+        self.previous_bidder = Txn.sender
+
+    # Bids
+    @abimethod
+    def bid(self, pay: gtxn.PaymentTransaction) -> None:  # đấu giá
+        # Check auction is ended or not
+        assert Global.latest_timestamp <= self.end_time, "Auction ended"
+
+        # verify payment
+        assert Txn.sender == self.previous_bidder, "Bidder must be different"
+        assert Txn.sender == pay.sender, "Bidder must be the sender"
+        assert pay.amount > self.asa_amount, "Bid must be greater than the start price"
+
+        # set data on global state
+        self.asa_amount = pay.amount
+        self.previous_bidder = Txn.sender
+
+        # update claimble amount
+        self.claimble[Txn.sender] = pay.amount
+
+    # Claim Bids
+    @abimethod
+    def claim_asset(self, asset: Asset) -> None:  # chuyển sản phẩm đấu giá cho người thắng
+        assert Txn.sender() == Global.creator_address, "Only the creator can access"
+        assert Global.latest_timestamp > self.end_time, "Auction not ended"
+
+        itxn.AssetTransfer(
+            xfer_asset=asset,
+            asset_receiver=self.previous_bidder,
+            asset_close_to=self.previous_bidder,
+            asset_amount=self.asa_amount,
+        ).submit()
+
+    @abimethod
+    def clam_bids(self) -> None:
+        amount = original_amount = self.claimble[Txn.sender]  # Lấy được token
+
+        if Txn.sender == self.previous_bidder:
+            amount -= self.asa_amount
+        itxn.Payment(amount=amount, receiver=Txn.sender).submit()
+
+        self.claimble[Txn.sender] = original_amount - amount
+
+    # Delete Application
+    @abimethod(allow_actions=["DeleteApplication"])
+    def delete_application(self) -> None:
+        itxn.Payment(
+            close_remainder_to=Global.creator_address,
+            receiver=Global.creator_address,
+        ).submit()
--- projects/auction2/tests/auction_test.py
+++ projects/auction2/tests/auction_test.py
@@ -1,3 +1,4 @@
+import time
 from collections.abc import Generator
 
 import pytest
@@ -13,13 +14,76 @@ def context() -> Generator[AlgopyTestContext, None, None]:
         ctx.reset()
 
 
-def test_hello(context: AlgopyTestContext) -> None:
-    # Arrange
-    dummy_input = context.any_string()
+def test_opt_into_asset(context: AlgopyTestContext):
+    asset = context.any_asset()
     contract = Auction()
 
-    # Act
-    output = contract.hello(dummy_input)
+    # Call opt_into_asset from the context
+    contract.opt_into_asset(asset)
 
-    # Assert
-    assert output == f"Hello, {dummy_input}"
+    assert contract.asa.id == asset.id
+
+    last_itxn = context.last_submitted_itxn.asset_transfer
+
+    assert last_itxn.xfer_asset == asset
+    assert last_itxn.asset_receiver == context.default_application.address
+
+
+def test_start_auction(context: AlgopyTestContext):
+    current_time = context.any_uint64(3, 9990)
+    duration = context.any_uint64(1, 100)
+    start_price = context.any_uint64(1, 400)
+
+    contract = Auction()
+    itxn_transfer = context.any_asset_transfer_transaction(
+        asset_receiver=context.default_application.address, asset_amount=start_price
+    )
+    context.patch_global_fields(latest_timestamp=current_time)
+
+    context.patch_txn_fields(sender=context.default_creator)
+    contract.asa_amount = start_price
+    contract.start_auction(start_price=start_price, length=duration, axfer=itxn_transfer)
+
+    assert contract.asa_amount == start_price
+    assert contract.end_time == current_time + duration
+    assert contract.previous_bidder == context.default_creator
+
+
+def test_bid(context: AlgopyTestContext):
+    end_auction = context.any_uint64(min_value=int(time.time() + 10000))
+    start_price = context.any_uint64(1, 100)
+    account = context.default_creator
+    pay_amount = context.any_uint64(int(start_price + 1), 1000)
+
+    contract = Auction()
+
+    contract.end_time = end_auction
+    contract.previous_bidder = account
+    contract.asa_amount = start_price
+    pay = context.any_payment_transaction(sender=account, amount=pay_amount)
+    contract.bid(pay)
+
+    assert contract.asa_amount == pay_amount
+    assert contract.previous_bidder == account
+    assert contract.claimble[account] == pay_amount
+
+
+def test_clam_bids(context: AlgopyTestContext):
+    account = context.any_account()
+    context.patch_txn_fields(sender=account)
+    contract = Auction()
+    claimable_amount = context.any_uint64()
+
+    contract.claimble[account] = claimable_amount
+    contract.previous_bidder = account
+    previous_bid = context.any_uint64(max_value=int(claimable_amount))
+    contract.asa_amount = previous_bid
+
+    contract.clam_bids()
+
+    amount = claimable_amount - previous_bid
+    last_itxn = context.last_submitted_itxn.payment
+
+    assert last_itxn.amount == amount
+    assert last_itxn.receiver == account
+    assert contract.claimble[account] == claimable_amount - amount
